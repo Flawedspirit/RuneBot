79de04d4042c41f945c453148d2ea9390e2d8126
Flawedspirit <me@flawedspirit.ca>
2020-10-21 15:11:44 -0400

    Added invite link command

---
+++ commands/invite.js
@@ -0,0 +1,19 @@
+/* REQUIRED FILES */
+const reload    = require('require-reload');
+
+/* REQUIRED FILES */
+const config    = reload('../config.json');
+
+module.exports = {
+    name: 'invite',
+    desc: 'Creates an invite link for this bot',
+    aliases: ['oauth', 'join'],
+    cooldown: 10,
+    execute(message) {
+        if(config.inviteLink) {
+            message.channel.send(`Add me to a server! ${config.inviteLink}\nMake sure you're logged in.`);
+        } else {
+            message.channel.send('No invite link defined.');
+        }
+    }
+}
